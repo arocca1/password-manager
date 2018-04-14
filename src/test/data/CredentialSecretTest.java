dba416f31365b56564fabd8a83f9dca19e002094
antonio <rocca1.antonio@gmail.com>
2018-04-14 14:11:25 -0400

    Add test for CredentialSecret
    AnswerTest and PasswordTest only test the getHiddenSecret method. No need for them.
    I will add those test files back once I write more functionality.

--- src/test/data/CredentialSecretTest.java
+++ src/test/data/CredentialSecretTest.java
@@ -1,14 +1,14 @@
 package test.data;
 
 import org.junit.Test;
-import main.data.Password;
+import main.data.CredentialSecret;
 import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 
-public class PasswordTest {
+public class CredentialSecretTest {
 	@Test
-	public void testHiddenAnswer() {
-		String hiddenAnswer = Password.getHiddenPassword();
+	public void testHiddenSecret() {
+		String hiddenAnswer = CredentialSecret.getHiddenSecret();
 		char[] chars = new char[hiddenAnswer.length()];
 		Arrays.fill(chars, '*');
 		assertEquals(new String(chars), hiddenAnswer);
